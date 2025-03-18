0796cbbe0a99dc5714bcf6165c8f849e338c73af
Anirudh <anirudhmounasamy@gmail.com>
2025-03-18 15:39:26 +0530

    completed day 1

---
+++ day_1_band_name_generator/main.py
@@ -0,0 +1,4 @@
+# This is a simple code that asks two inputs from the user and outputs a band name based on the input.
+city_name = input("What's the name of the city you grew up in? ")
+pet_name = input("What's your pet name? ")
+print(f"Your band name could be {city_name} {pet_name}")
